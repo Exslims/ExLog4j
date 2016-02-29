28fd54c93daa108e9efdf5680c6dce97a382c370
Константин <exslims1@mail.ru>
2016-02-29 16:30:19 +0400

    fix xsd validator + added tests

--- exlog4j-parsers/src/main/java/com/home/exlog4j/parsers/validators/XSDValidator.java
+++ exlog4j-parsers/src/main/java/com/home/exlog4j/parsers/validators/XSDValidator.java
@@ -9,23 +9,21 @@ import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Paths;
 
 /**
  * Created by Константин on 29.02.2016.
  */
 public class XSDValidator {
-    public static boolean validateXMLSchema(String xmlPath){
-        try {
-            SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-            Schema schema = factory.newSchema(new File("exlogger.xsd"));
+    private static final String ROOT = Paths.get("src" , "test/java/com/home/exlog4j/").toAbsolutePath().toString();
+    private static final String XSD_PATH = Paths.get(ROOT + "/xsd/exlogger.xsd").toAbsolutePath().toString();
 
-            Validator validator = schema.newValidator();
-            validator.validate(new StreamSource(new File(xmlPath)));
-        }catch (SAXException | IOException e) {
-            e.printStackTrace();
-            return false;
-        }
+    public static boolean validateXMLSchema(String xmlPath, String xsdPath) throws SAXException, IOException {
+        SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+        Schema schema = factory.newSchema(new File(xsdPath));
 
+        Validator validator = schema.newValidator();
+        validator.validate(new StreamSource(new File(xmlPath)));
         return true;
 
     }
