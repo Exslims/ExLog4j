8abfa9fd4cb2dd7ab5a3fa829940e8295abe0dbe
MarenkovSergey <qnx1000@gmail.com>
2016-05-09 21:51:42 +0300

    Added  JavaDOC documentation for  not implemented parsers

--- src/main/java/com/home/exlog4j/parsers/json/JsonConfigParser.java
+++ src/main/java/com/home/exlog4j/parsers/json/JsonConfigParser.java
@@ -4,7 +4,9 @@ import com.home.exlog4j.config.ExConfig;
 import com.home.exlog4j.parsers.Parser;
 
 import java.util.HashMap;
-
+/**
+ * Implementation of {@link Parser} that used to parse JSON configuration
+ */
 public class JsonConfigParser implements Parser {
     private HashMap<String, ExConfig> configs = new HashMap<String, ExConfig>();
     public HashMap<String, ExConfig> getConfigs(String path) {
--- src/main/java/com/home/exlog4j/parsers/properties/PropertiesConfigParser.java
+++ src/main/java/com/home/exlog4j/parsers/properties/PropertiesConfigParser.java
@@ -5,6 +5,9 @@ import com.home.exlog4j.parsers.Parser;
 
 import java.util.HashMap;
 
+/**
+ * Implementation of {@link Parser} that used to parse configuration represented by property file
+ */
 public class PropertiesConfigParser implements Parser {
 
     private HashMap<String, ExConfig> configs = new HashMap<>();
