7c49eb6372a988e107f1c49354f4ebc7d0749df2
MarenkovSergey <qnx1000@gmail.com>
2016-05-12 20:13:48 +0300

    Removed ExLogger implementation tests

--- src/main/java/examples/Main.java
+++ src/main/java/examples/Main.java
@@ -5,10 +5,13 @@ import com.home.exlog4j.Logger;
 
 public class Main {
     public static void main(String[] args) {
+
         Logger logger = ExLogger.getLogger(Main.class , "application-profile");
+
         logger.debug("Hello , debug");
         for (int i = 0; i < 2000; i++) {
             logger.error("Error message" , new RuntimeException("Text exception"));
         }
+
     }
 }
